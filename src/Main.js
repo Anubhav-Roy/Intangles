393a0c7730736777472efb9c21ec81d5932c5684
anubhav.roy91@gmail.com <anubhav.roy91@gmail.com>
2020-01-08 14:09:09 +0530

    added data fetching

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,45 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+const data = require('../src/assets/data.json');
 
 export default class extends Component {
+  state = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+  };
+
+  componentDidMount = () => {
+    var counter = 0;
+
+    // for (var singlePoint in data) {
+    //   console.log(
+    //     counter,
+    //     ' ',
+    //     data[counter].loc.coordinates[0],
+    //     data[counter].loc.coordinates[1],
+    //   );
+
+    //   //   this.setState(prev => {
+    //   //     return {
+    //   //       latitude: data[counter].loc.coordinates[0],
+    //   //       longitude: data[counter].loc.coordinates[1],
+    //   //     };
+    //   //   });
+
+    //   counter++;
+    // }
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          provider={PROVIDER_GOOGLE}
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: data[0].loc.coordinates[0],
+            longitude: data[0].loc.coordinates[1],
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}></MapView>
